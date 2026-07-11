fbe85960187bc04643abed97305b648f26a38b0a
William Rodriguez <willy94ing@protonmail.com>
2026-07-11 18:30:49 -0400

    Change base path for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/LandingPage/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/WillysStudio/' : '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
